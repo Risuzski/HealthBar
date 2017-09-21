13001fdb605a9709bf4e7bb50c33273627aec8dd
Wyan <pixelartforever@gmail.com>
2017-09-21 15:18:18 -0500

    HealthBar
    Completed HealthBar

--- MyWorld.java
+++ MyWorld.java
@@ -18,9 +18,10 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        
+
         addObject( new HealthBar( 1500, 2400, 25 ), getWidth()/2, getHeight()/2 );
         addObject( new Button( Color.GREEN, 50 ), getWidth()/2 - 200, getHeight()/2 + 100 );
         addObject( new Button( Color.RED, 80 ), getWidth()/2 + 200, getHeight()/2 + 100);
     }
+
 }
